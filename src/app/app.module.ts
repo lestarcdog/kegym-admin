776737f9d312032bcb5bdcc26ae70fa4a65b0258
lestarcdog <lestarcdog@yahoo.com>
2020-05-01 21:46:50 +0200

    Initial basic angular skeleton

---
+++ src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+
+import { AppRoutingModule } from './routing.module'
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
