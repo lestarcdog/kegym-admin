fb3198574d8b9169e070dbf0ec06a078c0c81248
Kovacs Takeshi <development@myaux.org>
2020-05-10 09:44:33 +0200

    Init firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,11 @@
+import * as admin from 'firebase-admin'
+import * as functions from 'firebase-functions'
+
+admin.initializeApp()
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+export const helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!")
+})
